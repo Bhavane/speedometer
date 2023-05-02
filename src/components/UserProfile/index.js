a7f5b11481eecb0d20ae69117a25ead9ca3eac5e
bhavane <bhavanejaya@gmail.com>
2023-05-02 17:27:24 +0000

    speedometer

---
+++ src/components/UserProfile/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const UserProfile = props => {
+  const {userDetails} = props
+  const {imageUrl, name, role} = userDetails
+  return (
+    <li className="user-card-container">
+      <img src={imageUrl} className="profile-pic" alt="profile-pic" />
+      <div className="user-details-container">
+        <h1 className="user-name"> {name} </h1>
+        <p className="user-designation"> {role} </p>
+      </div>
+      <button type="button" className="delete-button">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/cross-img.png"
+          alt="cross"
+          className="delete-img"
+        />
+      </button>
+    </li>
+  )
+}
+export default UserProfile
